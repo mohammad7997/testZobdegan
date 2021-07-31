78c5de048e3248493d8154f25db927e9759aae7e
mohammad7997 <mjarahi1379@gmail.com>
2021-07-31 22:48:25 +0430

    create UserSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'family' => $this->faker->lastName(),
+            'nationalId' => $this->faker->buildingNumber(10),
+            'phone' => $this->faker->buildingNumber(10),
+            'gender' => rand(0,1),
+            'address' => $this->faker->name(),
+            'userName' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // admin
+        User::factory()->create([
+            'userName'=>'admin',
+            'password'=>Hash::make('admin'),
+            'role'=>1
+        ]);
+
+        //client
+        User::factory()->count(10)->create();
+    }
+}
