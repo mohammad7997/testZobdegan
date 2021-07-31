063b178f6a2ef3fae30f0bc511d1beed2d47aff6
mohammad7997 <mjarahi1379@gmail.com>
2021-07-31 07:56:04 +0430

    create UserController and OrderController for admin

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    public function order()
+    {
+        $ordersCash = Order::query()->where([
+            'payStatus' => 1,
+        ])->get();
+
+        $installments = Order::query()->leftJoin('installment_pays', 'orders.id', '=', 'installment_pays.order_id')
+            ->orderBy(DB::raw('orders.id'), 'Desc')
+            ->where(DB::raw('orders.payMethod'), 0)
+            ->where(DB::raw('orders.payStatus'), 2)
+            ->select('orders.*', 'installment_pays.prepayment', 'installment_pays.id as idInstallment', 'installment_pays.timeOfInstallment', 'installment_pays.installmentPay', 'installment_pays.installmentNum')
+            ->get();
+
+    }
+}
